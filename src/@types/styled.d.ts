8b743d44a8bbad795dbea9ac5578a3352ff4f876
Walber <walber.araujo@getcandidly.com>
2024-01-08 22:27:50 -0300

    first commit with all initial configuration: router, default layout and styles, global theme and default theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import { defaultTheme } from "../styles/theme/defaultTheme";
+import "styled-components";
+
+
+type ThemeType = typeof defaultTheme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType{}
+}
